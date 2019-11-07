9602cffdc0837b340925ade058f0896ff8e768cd
Jeffrey Lansford <lansfordjr14@gmail.com>
2019-11-07 16:53:11 -0700

    added a Character Script with a Cube with it and moved .gitignore to the right place.

---
+++ Auto-Gerg/Assets/Scripts/Character.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    // varibles all characers use
+    [SerializeField]
+    private float healthMax;
+
+    [SerializeField]
+    private float attackDamage;
+
+    [SerializeField]
+    private float defence;
+
+    [SerializeField]
+    private int range;
+
+    [SerializeField]
+    private float attackSpeed;
+
+    public float health;
+
+
+
+    // functions for awake and accessing stats
+    private void Awake() {
+        health = healthMax;
+    }
+
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    public float getDefence() {
+        return defence;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public float getAttackSpeed() {
+        return attackSpeed;
+    }
+
+
+}
