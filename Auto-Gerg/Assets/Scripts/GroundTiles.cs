b64019470905dc21d2db1318bebe5c7565db5141
David <deduran@nmsu.edu>
2019-11-08 19:23:36 -0700

    Adding in the chessboard and two test pieces
    let me know if you find any problems with it

---
+++ Auto-Gerg/Assets/Scripts/GroundTiles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTiles : MonoBehaviour
+{
+    // Variables for the tiles
+
+    private bool characterOccupied = false;
+
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.transform.position;
+        //print(position);        testing to make sure that im getting correct position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
