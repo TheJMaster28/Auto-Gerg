78ac7ced2fcfaa01362eea4b9f7aed6189adeadc
Jeffrey Lansford <lansfordjr14@gmail.com>
2019-11-10 11:55:05 -0700

    created board script and trying to get rid of libaray pushes

---
+++ Auto-Gerg/Assets/Scripts/Board.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    public int rows;
+    public int colums;
+
+    
+    public GameObject[] tiles;
+
+    private void Start() {
+
+        tiles = new GameObject[rows * colums];
+
+        int tileIndex = 0;
+
+        for ( int i = 0; i < 8; i++) {
+            GameObject row = transform.GetChild(i).gameObject;
+
+            for ( int k = 0; k < 8; k++ ) {
+                tiles[tileIndex] = row.transform.GetChild(k).gameObject;
+                tileIndex++;
+            }
+
+        }
+
+        for (int i =0; i < tiles.Length; i++ ) {
+            print(tiles[i]);
+        }
+    }
+
+    
+}
+
+
+
