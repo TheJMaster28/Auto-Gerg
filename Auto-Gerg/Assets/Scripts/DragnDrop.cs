6d0aed4636da48a7eadb107ff89a1dcb8f29ba00
Jeffrey Lansford <lansfordjr14@gmail.com>
2019-11-12 17:25:25 -0700

    fixed merged conflict

--- Auto-Gerg/Assets/Scripts/DragnDrop.cs
+++ Auto-Gerg/Assets/Scripts/DragnDrop.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -39,45 +38,3 @@ public class DragnDrop : MonoBehaviour
         }
     }
 } // end of class
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DragnDrop : MonoBehaviour
-{
-    private GameObject _drag;
-    private Vector3 screenPosition;
-    private Vector3 offset;
-
-    private void Update()
-    {
-        if (_drag == null && Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 100.0f))
-            {
-                if (gameObject == hit.transform.gameObject)
-                {
-                    _drag = hit.transform.gameObject;
-                    screenPosition = Camera.main.WorldToScreenPoint(_drag.transform.position);
-                    offset = _drag.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z));
-                }
-            }
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            _drag = null;
-        }
-
-        if (_drag != null)
-        {
-            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
-            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + offset;
-            _drag.transform.position = new Vector3(currentPosition.x, _drag.transform.position.y, currentPosition.z);
-        }
-    }
-} // end of class
->>>>>>> Stashed changes
