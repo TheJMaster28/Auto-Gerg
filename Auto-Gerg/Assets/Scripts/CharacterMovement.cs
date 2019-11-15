ea3e2ce878cb643cf082fad4ac7f5828085163e4
David <deduran@nmsu.edu>
2019-11-14 17:22:41 -0700

    Unknown Changes
    Okay working

---
+++ Auto-Gerg/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Transform chessPiece;
+    public GameObject target;
+    
+    public float speed = 1f;
+    
+}
