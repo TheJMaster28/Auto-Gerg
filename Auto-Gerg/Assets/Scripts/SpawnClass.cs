0ee0ef69008733bc8fd2a8f48d24e1dbcc199e82
Anthony <anthonyjimenez1337@gmail.com>
2019-11-14 23:32:38 -0700

    Added Rough Implementation of Character Drag and Drop
    SpawnClass script controls spawning. Characters have no stats and only spawns a TestPiece1 on click.

---
+++ Auto-Gerg/Assets/Scripts/SpawnClass.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClass : MonoBehaviour
+{
+
+    public GameObject bladeMaster;
+    public GameObject Ranger;
+    public GameObject Gunslinger;
+    public Transform objectToMove;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void spawnBlademaster()
+    {
+        Debug.Log("Spawning bladeMaster");
+        Instantiate(bladeMaster, new Vector3(0, 1, 0), Quaternion.identity);
+
+    }
+
+    public void spawnGunslinger()
+    {
+        Debug.Log("Spawning gunslinger");
+        Instantiate(Gunslinger, new Vector3(0, 1, 0), Quaternion.identity);
+
+    }
+
+    public void spawnRanger()
+    {
+        Debug.Log("Spawning ranger");
+        Instantiate(Ranger, new Vector3(0, 1, 0), Quaternion.identity);
+
+    }
+
+}
