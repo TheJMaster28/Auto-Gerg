a9b4b25cf4fc6579d9570d913c8aa878d6ccda9b
Jeffrey Lansford <lansfordjr14@gmail.com>
2019-12-09 16:21:33 -0700

    added tool tips and round count script

---
+++ Auto-Gerg/Assets/Scripts/GetRoundCount.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetRoundCount : MonoBehaviour {
+
+    GameObject rm;
+    // Update is called once per frame
+    void Update () {
+        rm = GameObject.FindGameObjectWithTag ("RoundManager");
+        GetComponent<Text> ().text = "Round Count: " + rm.GetComponent<RoundManager> ().roundCount;
+    }
+}
