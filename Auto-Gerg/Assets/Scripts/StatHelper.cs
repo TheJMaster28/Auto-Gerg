fdd8f413e0abbee3415d62cb05ab3091d58286c8
Guillermo Avitia <gavitia@nmsu.edu>
2019-12-11 18:55:56 -0700

    Created New Stat Popup and added it to prefabs
    Recreated stat popup to be updated in real time and display the character stats on screen

---
+++ Auto-Gerg/Assets/Scripts/StatHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatHelper : MonoBehaviour
+{
+    public GameObject statCanvas;
+    public Text statText;
+    public bool isSet = false;
+
+
+    void Update()
+    {
+        statText.text =
+        "CharacterName: " + gameObject.name + "\n" +
+        "Class: " + gameObject.GetComponent<Character>().getClass() + "\n"+
+        "Health: " + gameObject.GetComponent<Character>().getHealth() + "/" + gameObject.GetComponent<Character>().getHealthMax() + "          " + 
+        "Armor: " + gameObject.GetComponent<Character>().getArmor() + "\n" +
+        "Attack Speed: " + gameObject.GetComponent<Character>().getAttackSpeed() + "        " +
+        "Resistance: " + gameObject.GetComponent<Character>().getResistance() + "\n" +
+        "Attack Damage: " + gameObject.GetComponent<Character>().getAttackType() + "        " +
+        "Attack Type: " + gameObject.GetComponent<Character>().getAttackType();
+    }
+    void OnMouseOver()
+    {
+        Debug.Log("Mouse Over Working");
+        if(Input.GetMouseButtonDown(1))
+        {
+            Debug.Log ("right click");
+            isSet = !isSet;
+            statCanvas.SetActive (isSet);
+        }
+    }
+}
